79a901223c2ff6da966e8ffa064078be7dc2d31e
Paul <paulberesuita@yanninas-iphone.gateway.2wire.net>
2013-10-04 02:21:11 -0400

    Vacation page enhancement

--- js/app.js
+++ js/app.js
@@ -1,13 +1,13 @@
 $.fn.toggleClick = function(){
 
-    var functions = arguments ;
+	var functions = arguments ;
 
-    return this.click(function(){
-            var iteration = $(this).data('iteration') || 0;
-            functions[iteration].apply(this, arguments);
-            iteration = (iteration + 1) % functions.length ;
-            $(this).data('iteration', iteration);
-    });
+	return this.click(function(){
+		var iteration = $(this).data('iteration') || 0;
+		functions[iteration].apply(this, arguments);
+		iteration = (iteration + 1) % functions.length ;
+		$(this).data('iteration', iteration);
+	});
 };
 
 $(document).ready(function(){
@@ -19,43 +19,53 @@ $(document).ready(function(){
 		function() {
 			$('#today-bubble').hide();
 		}
-	);
+		);
+
+	$( "#Save" ).click(function() { 
+
+		var dateSelected = $( "#timeDateBox" ).val();
+		$( "#ForecastDate" ).html( dateSelected );
+
+		// $( "#ForecastDate" ).add( "<span>Again</span>" ).appendTo($( "#ForecastDate" ));
+
+	});
+
 })
 
 angular.module('null', ['ui.bootstrap']);
 var DatepickerDemoCtrl = function ($scope, $timeout) {
-  $scope.today = function() {
-    $scope.dt = new Date();
-  };
-  $scope.today();
+	$scope.today = function() {
+		$scope.dt = new Date();
+	};
+	$scope.today();
 
-  $scope.showWeeks = true;
-  $scope.toggleWeeks = function () {
-    $scope.showWeeks = ! $scope.showWeeks;
-  };
+	$scope.showWeeks = true;
+	$scope.toggleWeeks = function () {
+		$scope.showWeeks = ! $scope.showWeeks;
+	};
 
-  $scope.clear = function () {
-    $scope.dt = null;
-  };
+	$scope.clear = function () {
+		$scope.dt = null;
+	};
 
   // Disable weekend selection
   $scope.disabled = function(date, mode) {
-    return ( mode === 'day' && ( date.getDay() === 0 || date.getDay() === 6 ) );
+  	return ( mode === 'day' && ( date.getDay() === 0 || date.getDay() === 6 ) );
   };
 
   $scope.toggleMin = function() {
-    $scope.minDate = ( $scope.minDate ) ? null : new Date();
+  	$scope.minDate = ( $scope.minDate ) ? null : new Date();
   };
   $scope.toggleMin();
 
   $scope.open = function() {
-    $timeout(function() {
-      $scope.opened = true;
-    });
+  	$timeout(function() {
+  		$scope.opened = true;
+  	});
   };
 
   $scope.dateOptions = {
-    'year-format': "'yy'",
-    'starting-day': 1
+  	'year-format': "'yy'",
+  	'starting-day': 1
   };
 };
